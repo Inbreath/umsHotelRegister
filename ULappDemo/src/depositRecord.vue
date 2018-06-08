1164b701bc7dae19908adbce9d3f46f494cebf90
wupengwei <913780101@qq.com>
2018-06-08 11:47:11 +0800

    完成--此版打包给翟学远，完成主界面和记录查询界面两个界面的设计和开发

--- ULappDemo/src/depositRecord.vue
+++ ULappDemo/src/depositRecord.vue
@@ -8,28 +8,104 @@
                 @onRightPartClick="rightClick"
                 class="header"
                 backgroundColor="#a327eb">
-        <!-- <text slot="right" style="height:40px;width:100px;"@click="select">{{righttext}}</text> -->
         </ums-header>
-        <div style="position:relative; left:0px; background-color:#fff; height:100px; width:800;" ></div> 
+        <div style="position:relative; left:0px; background-color:#fff; height:110px; width:800;" ></div> 
         <div style="position:absolute; left:0px; top:140px; background-color:#a327eb; height:60px; width:800px;"></div>
-        <div style="position:absolute; left:50px; top:140px; border-width: 2px; border-radius: 20px; border-color: #000; background-color:#ffffff; width:660px; height:100px" >
+        <div style="position:absolute; left:45px; top:140px; border-width: 2px; border-radius: 20px; border-color: #ccc; background-color:#ffffff; width:660px; height:105px" >
         
         <div style="position:relative; left:0px; background-color:#ffffff; height:100px; width:660;" ></div> 
         <div style="position:absolute; left: 0px; top:25px; width:100px; height: 100px;">
                   <image style="left: 20px; width:50px; height: 50px;" src="./static/search.png"></image>
         </div>
-
         <div style="position:absolute; left:110px; top:0px; width:250px; height: 100px;">
                   <input style=" width:250px; height: 100px;" placeholder="请输入查询内容！" autofocus=false />
         </div>
-
         <div style="position:absolute; left:520px; top:25px; width:100px; height: 100px;"  >
                  <text style="width:100px; height: 100px; font-size:50px; color:#a327eb;" @click="keep" >查询</text>
         </div>      
         </div> 
 
-         <div style="position:relative; left:0px; top:32px; background-color:#000; height:850px; width:660;" ></div> 
-        	
+         <div style="position:relative; left:0px; top:32px; background-color:#fff; border-width: 2px; border-radius: 20px; border-color: #ccc;height:950px; width:660;" >
+              
+
+              <div style="position:absolute; left:5px; top:8px; background-color:#fff; width:650px; height:150px" >
+              <div style="position:relative; left:0px; background-color:#fff; height:150px; width:650px;" >
+              <div style="position:absolute; left:40px; top:20px;  width:100px; height:100px" >
+                   <image style='width:100; height:100; left:0px; top:0px;' src="./static/icon_record.png"></image>
+              </div>
+               <div style="position:absolute; left:150px; top:20px; background-color:#fff; width:400px; height:100px" >
+                   <text style="font-size:40px; color:#ccc;"> 用户名一</text>
+                   <text style="font-size:40px; color:#ccc;"> 123456789</text>
+              </div>
+              <div style="position:absolute; left:560px; top:20px;  width:100px; height:100px" >
+                   <image style='width:20; height:30; left:20px; top:30px;' src="./static/ic_arr.png"></image>
+              </div></div></div>
+
+              <!-- top:158px;308px;458px;608px;758px; 需要在下面这行中进行设置 -->
+              <div style="position:absolute; left:5px; top:158px; background-color:#fff; width:650px; height:150px" >
+              <div style="position:relative; left:0px; background-color:#fff; height:150px; width:650px;" >
+              <div style="position:absolute; left:40px; top:20px;  width:100px; height:100px" >
+                   <image style='width:100; height:100; left:0px; top:0px;' src="./static/icon_record.png"></image>
+              </div>
+               <div style="position:absolute; left:150px; top:20px; background-color:#fff; width:400px; height:100px" >
+                   <text style="font-size:40px; color:#ccc;"> 用户名三</text>
+                   <text style="font-size:40px; color:#ccc;"> 123456789</text>
+              </div>
+              <div style="position:absolute; left:560px; top:20px;  width:100px; height:100px" >
+                   <image style='width:20; height:30; left:20px; top:30px;' src="./static/ic_arr.png"></image>
+              </div></div></div>
+              <div style="position:absolute; left:5px; top:308px; background-color:#fff; width:650px; height:150px" >
+              <div style="position:relative; left:0px; background-color:#fff; height:150px; width:650px;" >
+              <div style="position:absolute; left:40px; top:20px;  width:100px; height:100px" >
+                   <image style='width:100; height:100; left:0px; top:0px;' src="./static/icon_record.png"></image>
+              </div>
+               <div style="position:absolute; left:150px; top:20px; background-color:#fff; width:400px; height:100px" >
+                   <text style="font-size:40px; color:#ccc;"> 用户名四</text>
+                   <text style="font-size:40px; color:#ccc;"> 123456789</text>
+              </div>
+              <div style="position:absolute; left:560px; top:20px;  width:100px; height:100px" >
+                   <image style='width:20; height:30; left:20px; top:30px;' src="./static/ic_arr.png"></image>
+              </div></div></div>
+              <div style="position:absolute; left:5px; top:458px; background-color:#fff; width:650px; height:150px" >
+              <div style="position:relative; left:0px; background-color:#fff; height:150px; width:650px;" >
+              <div style="position:absolute; left:40px; top:20px;  width:100px; height:100px" >
+                   <image style='width:100; height:100; left:0px; top:0px;' src="./static/icon_record.png"></image>
+              </div>
+               <div style="position:absolute; left:150px; top:20px; background-color:#fff; width:400px; height:100px" >
+                   <text style="font-size:40px; color:#ccc;"> 用户名五</text>
+                   <text style="font-size:40px; color:#ccc;"> 123456789</text>
+              </div>
+              <div style="position:absolute; left:560px; top:20px;  width:100px; height:100px" >
+                   <image style='width:20; height:30; left:20px; top:30px;' src="./static/ic_arr.png"></image>
+              </div></div></div>
+              <div style="position:absolute; left:5px; top:608px; background-color:#fff; width:650px; height:150px" >
+              <div style="position:relative; left:0px; background-color:#fff; height:150px; width:650px;" >
+              <div style="position:absolute; left:40px; top:20px;  width:100px; height:100px" >
+                   <image style='width:100; height:100; left:0px; top:0px;' src="./static/icon_record.png"></image>
+              </div>
+               <div style="position:absolute; left:150px; top:20px; background-color:#fff; width:400px; height:100px" >
+                   <text style="font-size:40px; color:#ccc;"> 用户名六</text>
+                   <text style="font-size:40px; color:#ccc;"> 123456789</text>
+              </div>
+              <div style="position:absolute; left:560px; top:20px;  width:100px; height:100px" >
+                   <image style='width:20; height:30; left:20px; top:30px;' src="./static/ic_arr.png"></image>
+              </div></div></div>
+              <div style="position:absolute; left:5px; top:758px; background-color:#fff; width:650px; height:150px" >
+              <div style="position:relative; left:0px; background-color:#fff; height:150px; width:650px;" >
+              <div style="position:absolute; left:40px; top:20px;  width:100px; height:100px" >
+                   <image style='width:100; height:100; left:0px; top:0px;' src="./static/icon_record.png"></image>
+              </div>
+               <div style="position:absolute; left:150px; top:20px; background-color:#fff; width:400px; height:100px" >
+                   <text style="font-size:40px; color:#ccc;"> 用户名七</text>
+                   <text style="font-size:40px; color:#ccc;"> 123456789</text>
+              </div>
+              <div style="position:absolute; left:560px; top:20px;  width:100px; height:100px" >
+                   <image style='width:20; height:30; left:20px; top:30px;' src="./static/ic_arr.png"></image>
+              </div></div></div>
+
+              
+   
+        </div> 
             <scroller>
                 <div v-for="item in this.textqurry" :key="item" class="order-item">
                     <text class="text-no2">{{ item.deposit_id}}</text>
